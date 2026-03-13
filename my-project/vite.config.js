ce59db6ce7926373090ecaf4747c62229a048773
Amreen Fathima A <fathimaamreen351@gmail.com>
2026-03-13 11:37:53 +0530

    config file updated

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,35 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
   ],
+
+  server: {
+    host: true,
+    allowedHosts: [
+      "shop-with-niya.sbbs.co.in"
+    ]
+  },
+
   build: {
-    // Enable code splitting
     rollupOptions: {
       output: {
-        // Manual chunk splitting for better caching
         manualChunks: {
-          // Vendor chunk for large dependencies
           vendor: ['react', 'react-dom', 'react-router-dom'],
-          // Firebase chunk
           firebase: ['firebase/app', 'firebase/auth'],
-          // UI libraries chunk
           ui: ['@heroicons/react', 'lucide-react', 'react-hot-toast', 'react-toastify'],
-          // Redux chunk
           redux: ['@reduxjs/toolkit', 'react-redux'],
-          // Utils chunk
           utils: ['axios', 'react-helmet-async']
         }
       }
     },
-    // Increase chunk size warning limit
     chunkSizeWarningLimit: 1000,
-    // Enable source maps for debugging (can be disabled in production)
     sourcemap: false,
-    // Minify for smaller bundles
     minify: 'terser',
     terserOptions: {
       compress: {
@@ -38,7 +35,7 @@ export default defineConfig({
       }
     }
   },
-  // Optimize dependencies
+
   optimizeDeps: {
     include: [
       'react',
